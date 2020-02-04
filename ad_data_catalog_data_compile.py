42e70c1a5506f4f00ff4b0e450911b4868ae79c8
Attila Kiss <atttila.kiss@gmail.com>
2020-02-04 12:24:04 +0100

    data_compiling and json_saving classes
    - data compilation is now in a separate class
    - json saving is now in separate class

---
+++ ad_data_catalog_data_compile.py
@@ -0,0 +1,24 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+import sqlite3
+import re
+import sys
+from datetime import *
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+class FullData:
+    def __init__(self):
+        self.full_data = dict()
+
+    def full_data_compile(self, ad_url, advertisement_data, catalog_url, catalog_data):
+        """
+        compiling the available data into one dictionary, and adds the URLs
+        """
+        self.full_data['ad_URL'] = ad_url
+        for k, v in advertisement_data.items():
+            self.full_data[k] = v
+
+        self.full_data['catalog_url'] = catalog_url
+        for k, v in catalog_data.items():
+            self.full_data[k] = v
